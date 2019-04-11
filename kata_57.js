590690679d50228719ec0c2596f9f985b98b43e6
clintsearl <clintsearl@gmail.com>
2019-04-11 13:42:53 -0600

    finished 57, 58, and started 59 and 75

--- kata_57.js
+++ kata_57.js
@@ -4,21 +4,25 @@
 
 describe('Default parameters make function parameters more flexible', () => {
   it('define it using an assignment to the parameter `function(param=1){}`', function() {
-    let number = (int) => int;
+    //Assigned the int when passing it in the function
+    let number = (int=0) => int;
     assert.equal(number(), 0);
   });
   it('it is used when `undefined` is passed', function() {
     let number = (int = 23) => int;
-    const param =undefined;
+    const param= undefined;
+    // let param this works too, it uses the default that is assigned in the function
     assert.equal(number(param), 23);
   });
   it('it is not used when a value is given', function() {
-    function xhr() {
+    // Called the param being passed in method, so it could return it.
+    function xhr(method) {
       return method;  
     }
     assert.equal(xhr('POST'), 'POST');
   });
   it('it is evaluated at run time', function() {
+    //had to define the defaultValue asert wanted 42
     let defaultValue= 42;
     function xhr(method = `value: ${defaultValue}`) {
       return method;  
@@ -26,12 +30,11 @@ describe('Default parameters make function parameters more flexible', () => {
     assert.equal(xhr(), 'value: 42');
   });
   it('it can also be a function', function() {
-    const defaultValue = 0;
-    function fn(value = defaultValue()(
-      defaultValue +1) 
- 
-      return value;
-    )}
+    //turned it into a function returning the string it wanted.
+    const defaultValue = () =>{return 'defaultValue'};
+    function fn(value = defaultValue()) {
+      return value;  
+    }
     assert.equal(fn(), 'defaultValue');
   });
 });
---
+++ kata_58.js
@@ -0,0 +1,39 @@
+// 58: Reflect - basics
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Reflect` basics', function() {
+    describe('Reflect is special, it is different to e.g. `Object`', function() {
+      it('it`s of type object', function() {
+        const expectedType = 'object';
+        assert.equal(typeof Reflect, expectedType);
+      });
+      it('it can not be instantiated (`new Reflect()`)', function() {
+        //this doesn't create a new one... assert proves TypeError
+        const tryToConstruct = () => { new Reflect(); };
+        assert.throws(tryToConstruct, TypeError);
+      });
+      it('has no `call` method (as opposed to e.g. Object)', function() {
+        //reflect cannot be called like a finction or an object so it doesn't have a call
+        const expected = "undefined";
+        assert.equal(typeof Reflect.call, expected);
+      });
+    });
+    
+    describe('some `Reflect` usages', function() {
+      it('`Reflect.construct()` is like `new ClassName`', function() {
+        //made a new class called Class and it is similar to reflect  in that they are consturctors
+        let Class = class {}
+        assert.equal(Reflect.construct(Class, []) instanceof Class, true);
+      });
+      it('`Reflect.get()` returns a property`s value', function() {
+          //change the object so it was what it's looking for, 
+        let obj = {x: 23};
+        assert.equal(Reflect.get(obj, 'x'), 23);
+      });
+      it('`Reflect.has()` is like `in` just as a function', function() {
+          //added the key and value into the object so it can see if it's there it looks for the key:
+        let obj = {x: 'y'};
+        assert.equal(Reflect.has(obj, 'x'), true);
+      });
+    });
+  });
